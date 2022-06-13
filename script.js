974f5b07839399960ce2965b30a33e8564dc21d7
Foxy-FnF <91306148+Foxy-FnF@users.noreply.github.com>
2022-06-12 19:46:10 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var variable1;
+variable1 = window.prompt('Digiteme el valor para la  constante 1: ');
+
+var varibale2; 
+variable2 = window.prompt('Digitame el valor para la constante 2: ');
+
+
+console.log('El valor numerico de la constante 1 es: ' + variable1 );
+console.log('El valor numerico de la constante 2 es: ' + variable2 );
+
+variable1 = window.prompt('Ingrese el nuevo valor para la  constante 1: ');
+variable2 = window.prompt('Ingrese el nuevo valor para la constante 2: ');
+
+console.log('El nuevo valor numerico de la constante 1 es: ' + variable2 );
+console.log('El nuevo valor numerico de la constante 2 es: ' + variable2 );
+
+const objetos = ["Miguel Enrique", "Alvarez Merlos", "Morales Izabal", 21];
+
+console.log(objetos);
